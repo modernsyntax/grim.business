ad6caaea880317f01b87723d852dc8e55bcfe903
brionisonline <brionis.online@outlook.com>
2019-05-09 23:00:27 -0700

    added styling to the social drawers

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,9 @@
 function toggleFb() {
     document.getElementById("twtr-drawer").style.transitionDelay = "0ms";
     document.getElementById("fb-drawer").style.transitionDelay = "0ms";
-    document.getElementById("fb-drawer").classList.toggle("active")
-    if ( document.getElementById("twtr-drawer").classList.contains("active")) {
-        document.getElementById("twtr-drawer").classList.remove("active")
+    document.getElementById("fb-drawer").classList.toggle("active");
+    if (document.getElementById("twtr-drawer").classList.contains("active")) {
+        document.getElementById("twtr-drawer").classList.remove("active");
         document.getElementById("fb-drawer").style.transitionDelay = "500ms";
     }
 }
@@ -11,9 +11,9 @@ function toggleFb() {
 function toggleTw() {
     document.getElementById("fb-drawer").style.transitionDelay = "0ms";
     document.getElementById("twtr-drawer").style.transitionDelay = "0ms";
-    document.getElementById("twtr-drawer").classList.toggle("active")
-    if ( document.getElementById("fb-drawer").classList.contains("active")) {
-        document.getElementById("fb-drawer").classList.remove("active")
+    document.getElementById("twtr-drawer").classList.toggle("active");
+    if (document.getElementById("fb-drawer").classList.contains("active")) {
+        document.getElementById("fb-drawer").classList.remove("active");
         document.getElementById("twtr-drawer").style.transitionDelay = "500ms";
     }
 }
