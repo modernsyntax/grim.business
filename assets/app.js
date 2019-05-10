b5b7ac7793781d767f111ece6f2565a1ebddc7a1
brionisonline <brionis.online@outlook.com>
2019-05-10 01:07:57 -0700

    final version before major change

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,15 @@ function toggleFb() {
     }
 }
 
+function colorFb() {
+    document.getElementById("facebookicon").setAttribute("src" , "assets/images/fbcolor.png")
+}
+
+function decolorFb() {
+    document.getElementById("facebookicon").setAttribute("src" , "assets/images/facebook.png")
+}
+
+
 function toggleTw() {
     document.getElementById("fb-drawer").style.transitionDelay = "0ms";
     document.getElementById("twtr-drawer").style.transitionDelay = "0ms";
@@ -16,4 +25,11 @@ function toggleTw() {
         document.getElementById("fb-drawer").classList.remove("active");
         document.getElementById("twtr-drawer").style.transitionDelay = "500ms";
     }
+}
+function colorTw() {
+    document.getElementById("twittericon").setAttribute("src" , "assets/images/twcolor.png")
+}
+
+function decolorTw() {
+    document.getElementById("twittericon").setAttribute("src" , "assets/images/twitter.png")
 }
